bb8191ebca7d223f1987c813a4e2e9b99ebfe916
WansleiAraujo <36192039+WansleiAraujo@users.noreply.github.com>
2023-02-18 20:40:06 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Digite um texto: ");
+string texto = Console.ReadLine();
+
+int qPalavras = texto.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
+int qCaracteres = texto.Length;
+
+Console.WriteLine(qCaracteres + " caracteres, " + qPalavras + " palavras");
---
+++ Python/main.py
@@ -0,0 +1,6 @@
+texto = input("Digite um texto: ")
+
+qPalavras = len(texto.split())
+qCaracteres = len(texto)
+
+print(f"{qCaracteres} caracteres, {qPalavras} palavras")
